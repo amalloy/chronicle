cf450e1cae0feeda4513c8b4beeb402860583216
Alan Malloy <alan@malloys.org>
2013-04-26 13:36:20 -0700

    Make times-for work

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/flatland/chronicle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.flatland/useful "0.10.0"]
+                 [clj-time "0.5.0"]])
--- src/flatland/chronicle.clj
+++ src/flatland/chronicle.clj
@@ -1,6 +1,40 @@
-(ns flatland.chronicle)
+(ns flatland.chronicle
+  (:require [clj-time.core :as time :refer [minutes]]
+            [clj-time.periodic :refer [periodic-seq]]))
+
+(def extractors
+  {:minute time/minute
+   :hour time/hour
+   :day time/day
+   :month time/month
+   :day-of-week time/day-of-week})
+
+(def default-spec
+  {:minute (range 60)
+   :hour (range 24)
+   :day (range 1 32)
+   :month (range 1 13)
+   :day-of-week (range 1 8)})
+
+(defn make-spec [spec]
+  (into {} (for [[k v] (merge default-spec spec)]
+             [k (set v)])))
 
 (defn time-match?
   "Determine if the given time matches the cron spec."
   [spec time]
-  true)
+  (every? true?
+          (for [[k v] extractors]
+            (contains? (get spec k) (v time)))))
+
+(defn round-time [time]
+  (time/date-time (time/year time)
+                  (time/month time)
+                  (time/day time)
+                  (time/hour time)
+                  (time/minute time)))
+
+(defn times-for [spec start]
+  (for [time (periodic-seq (round-time start) (minutes 1))
+        :when (time-match? spec time)]
+    time))
